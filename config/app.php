10705b420cffed5e96161bdd0fa9a2863c69d066
sinur <3s0c9m7@gmail.com>
2022-06-09 14:19:58 +0700

    add library: laravel pdf merger from webklex

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Webklex\PDFMerger\Providers\PDFMergerServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -212,6 +213,7 @@ return [
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
+        'PDFMerger' => Webklex\PDFMerger\Facades\PDFMergerFacade::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
         'Redis' => Illuminate\Support\Facades\Redis::class,
