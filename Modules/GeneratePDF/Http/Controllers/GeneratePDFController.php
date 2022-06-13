b2d04b238d3417c4e2d2bbbfc09a85fa5485d2d0
sinur <3s0c9m7@gmail.com>
2022-06-13 13:42:53 +0700

    tes convert using libre office

--- Modules/GeneratePDF/Http/Controllers/GeneratePDFController.php
+++ Modules/GeneratePDF/Http/Controllers/GeneratePDFController.php
@@ -6,9 +6,11 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
-use Webklex\PDFMerger\Facades\PDFMergerFacade as PDFMerger;
+use PDF;
+use PDFMerger;
 use setasign\Fpdi\Fpdi;
-use setasign\Fpdi\PdfReader;
+//use setasign\Fpdi\PdfReader;
+use NcJoes\OfficeConverter\OfficeConverter;
 
 class GeneratePDFController extends Controller
 {
@@ -21,17 +23,23 @@ class GeneratePDFController extends Controller
     public function index(Request $request)
     {
         if ( $request->merge ) {
-
             $this->merger()
                  ->mergedPDF($request->pdf_portrait)
                  ->mergedPDF($request->pdf_landscape)
                  ->mergedPDF($request->pdf_landscape_portrait)
+                 ->mergedPDF('doc/portrait.docx', 'doc')
+                 ->mergedPDF('doc/landscape.docx', 'doc')
+                 ->mergedPDF('doc/landscape_portrait.docx', 'doc')
                  ->stream();
 
         }
         
         return view('generatepdf::index');
     }
+
+
+
+    // Merge PDF
     public function merger()
     {
         $this->oMerger = PDFMerger::init();
@@ -44,8 +52,15 @@ class GeneratePDFController extends Controller
 
         return $this->oMerger->stream();
     }
-    public function mergedPDF( $fileName )
+    public function mergedPDF( $fileName , $type=false)
     {
+        if ( $type ) {
+            if ( $type == 'doc' ) {
+                $this->convertWordToPDF($fileName);
+                $fileName = $this->getNameFileNoExtension($fileName).'.pdf';
+            }
+        }
+
         $pdf = new Fpdi();
 
         // set the source file
@@ -72,6 +87,74 @@ class GeneratePDFController extends Controller
             
         return $this;
     }
+
+
+
+
+
+    /* Convert Word To PDF
+     */
+    public function convertWordToPDF($fileName)
+    {
+        $converter = new OfficeConverter($fileName);
+        $converter->convertTo(public_path('output-file.pdf'));
+        dd($converter);
+
+        $fileNameNoExtension = $this->getNameFileNoExtension($fileName);
+
+        /* Set the PDF Engine Renderer Path */
+        $domPdfPath = base_path('vendor/dompdf/dompdf');
+        \PhpOffice\PhpWord\Settings::setPdfRendererPath($domPdfPath);
+        \PhpOffice\PhpWord\Settings::setPdfRendererName('DomPDF');
+         
+        /*@ Reading doc file */
+        //$template = new\PhpOffice\PhpWord\TemplateProcessor(public_path($fileName));
+ 
+        /*@ Replacing variables in doc file */
+        //$template->setValue('date', date('d-m-Y'));
+        //$template->setValue('title', 'Mr.');
+        //$template->setValue('firstname', 'Scratch');
+        //$template->setValue('lastname', 'Coder');
+
+        /*@ Save Temporary Word File With New Name */
+        //$saveDocPath = public_path($fileNameNoExtension.'.docx');
+        //$template->saveAs($saveDocPath);
+         
+        // Load temporarily create word file
+        //$Content = \PhpOffice\PhpWord\IOFactory::load($saveDocPath); 
+        $Content = \PhpOffice\PhpWord\IOFactory::load($fileName); 
+ 
+        //Save it into PDF
+        $savePdfPath = public_path($fileNameNoExtension.'.pdf');
+ 
+        /*@ If already PDF exists then delete it */
+        //if ( file_exists($savePdfPath) ) {
+        //    unlink($savePdfPath);
+        //}
+ 
+        //Save it into PDF
+        $PDFWriter = \PhpOffice\PhpWord\IOFactory::createWriter($Content,'PDF');
+        $PDFWriter->save($savePdfPath); 
+       // echo 'File has been successfully converted';
+ 
+        /*@ Remove temporarily created word file */
+        //if ( file_exists($saveDocPath) ) {
+        //    unlink($saveDocPath);
+        //}
+    }
+
+    public function getNameFileNoExtension($fileName)
+    {
+        return preg_replace('/\\.[^.\\s]{3,4}$/', '', $fileName);
+    }
+
+
+
+
+
+
+
+
 
     /**
      * Show the form for creating a new resource.
--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Webklex\PDFMerger\Providers\PDFMergerServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -213,6 +214,7 @@ return [
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
         'PDFMerger' => Webklex\PDFMerger\Facades\PDFMergerFacade::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
